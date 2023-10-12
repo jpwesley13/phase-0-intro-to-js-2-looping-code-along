166cb7294ed3233a21869e2a4b9aae903ac33899
Jared Patterson <jpwesley34@gmail.com>
2023-10-11 17:26:35 -0700

    receives gifts

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+const giftMessage = []
+//const girls = ["Ali", "Set", "Aurora"]
+//const occasion = "Halloween"
+function writeCards(girls, occasion) {
+    for (let CG = 0; CG < girls.length; CG++) {
+        giftMessage.push(`Thank you, ${girls[CG]}, for the wonderful ${occasion} gift!`);
+        
+    }
+    return giftMessage
+}
+
+let n = true
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--)
+    }
+}
